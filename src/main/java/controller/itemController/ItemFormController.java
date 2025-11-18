91e1ea9e6149775ca73732c344121c6f43ed8e72
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2025-11-18 13:05:43 +0530

    add ItemFormController with UI components and action handlers

--- src/main/java/controller/itemController/ItemFormController.java
+++ src/main/java/controller/itemController/ItemFormController.java
@@ -1,4 +1,64 @@
 package controller.itemController;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class ItemFormController {
+
+    @FXML
+    private TableColumn<?, ?> colDescription;
+
+    @FXML
+    private TableColumn<?, ?> colPackSize;
+
+    @FXML
+    private TableColumn<?, ?> colQtyOnHand;
+
+    @FXML
+    private TableColumn<?, ?> colUnitPrice;
+
+    @FXML
+    private TableColumn<?, ?> colitemCode;
+
+    @FXML
+    private TableView<?> tblItemView;
+
+    @FXML
+    private TextField txtDescription;
+
+    @FXML
+    private TextField txtItemCode;
+
+    @FXML
+    private TextField txtPackSize;
+
+    @FXML
+    private TextField txtQtyOnHand;
+
+    @FXML
+    private TextField txtUnitPrice;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
 }
