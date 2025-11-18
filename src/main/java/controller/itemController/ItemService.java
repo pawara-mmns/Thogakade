76850e1ee499fc2bb66a87611d066eb49b5f1524
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2025-11-18 13:07:54 +0530

    add methods for item management in ItemService interface

--- src/main/java/controller/itemController/ItemService.java
+++ src/main/java/controller/itemController/ItemService.java
@@ -1,4 +1,15 @@
 package controller.itemController;
 
+import javafx.collections.ObservableList;
+import model.dto.ItemDTO;
+
 public interface ItemService {
+
+    void addItemDetails(String itemName, String itemCode, double itemPrice, int itemQuantity);
+
+    void deleteItemDetails(String itemCode);
+
+    void updateItemDetails(String itemName, String itemCode, double itemPrice, int itemQuantity);
+
+    ObservableList<ItemDTO> getAllItemDetails();
 }
