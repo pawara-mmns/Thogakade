abd7f47c61c3604de2eed2f44059454c82eb73c4
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2025-11-18 12:54:38 +0530

    add buttons and event handlers for customer, item, and order management

--- src/main/java/controller/dashboard/DashBoardFormController.java
+++ src/main/java/controller/dashboard/DashBoardFormController.java
@@ -1,4 +1,52 @@
 package controller.dashboard;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DashBoardFormController {
+
+    Stage stage = new Stage();
+
+    @FXML
+    private Button btnCustoemerManagement;
+
+    @FXML
+    private Button btnItemManagement;
+
+    @FXML
+    private Button btnOrderDetailsManagement;
+
+    @FXML
+    private Button btnOrderManagement;
+
+    @FXML
+    void btnCustoemerManagementOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemManagementOnAction(ActionEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(""))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void btnOrderDetailsManagementOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderManagementOnAction(ActionEvent event) {
+
+    }
+
 }
