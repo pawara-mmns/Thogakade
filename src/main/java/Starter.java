85f52e118de1ed9595756c4b76889b8f1194df5d
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2025-11-18 12:06:32 +0530

    initial commit

---
+++ src/main/java/Starter.java
@@ -0,0 +1,11 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(""))));
+    }
+}
