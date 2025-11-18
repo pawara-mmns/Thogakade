84c2be7a042230346068b14ed08d7e1e4b73127e
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2025-11-18 12:53:55 +0530

    implement application entry point and initialize dashboard scene

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -4,8 +4,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Starter extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(""))));
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/dashboard.fxml"))));
+        stage.show();
     }
 }
