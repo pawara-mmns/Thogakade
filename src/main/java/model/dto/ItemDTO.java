7cca09ce7074707a8953ac7e9733af5fc3f13df9
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2025-11-18 13:05:50 +0530

    add ItemDTO class for item data transfer

---
+++ src/main/java/model/dto/ItemDTO.java
@@ -0,0 +1,19 @@
+package model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemDTO {
+
+    private String itemCode;
+
+    private String description;
+
+    private double unitPrice;
+
+    private int qtyOnHand;
+}
