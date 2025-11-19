3c48deaec59a90cdc9c54b88e6b1a8a0b93a5ea0
pawara-mmns <pawarasamarawickrama@gmail.com>
2025-11-19 12:14:35 +0530

    update database connection credentials in DBConnection to use the correct password

--- src/main/java/db/DBConnection.java
+++ src/main/java/db/DBConnection.java
@@ -11,7 +11,7 @@ public class DBConnection {
     private Connection connection;
 
     private DBConnection() throws SQLException {
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade","root","12345");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade","root","1234");
     }
 
     public static DBConnection getInstance() throws SQLException {
