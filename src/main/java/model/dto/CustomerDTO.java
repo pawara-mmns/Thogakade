28701f97ba218bf371e0d083a1de7f67d173b8d0
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2025-11-20 12:17:21 +0530

    add CustomerDTO class to represent customer data

---
+++ src/main/java/model/dto/CustomerDTO.java
@@ -0,0 +1,24 @@
+package model.dto;
+
+import java.time.LocalDate;
+
+public class CustomerDTO {
+
+    private String customerId;
+
+    private String title;
+
+    private String name;
+
+    private LocalDate dateOfBirth;
+
+    private Double salary;
+
+    private String address;
+
+    private String city;
+
+    private String province;
+
+    private String postalCode;
+}
