971a23da77b83e68093236e82c89c96d60def288
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2025-11-26 11:15:17 +0530

    enhance CustomerRepository with delete and update methods; add searchCustomer method for customer retrieval

--- src/main/java/repository/CustomerRepository.java
+++ src/main/java/repository/CustomerRepository.java
@@ -7,9 +7,11 @@ public interface CustomerRepository {
 
     void addCustomer(String custID, String custTitle, String custName, String dob, double salary, String custAddress, String city, String province, String postalCode);
 
-    void deleteCustomer();
+    void deleteCustomer(String custID);
 
-    void updateCustomer();
+    void updateCustomer(String custID, String custTitle, String custName, String dob, double salary, String custAddress, String city, String province, String postalCode);
 
     ResultSet getAllCustomers() throws SQLException;
+
+    ResultSet searchCustomer(String custID, String custName) throws SQLException;
 }
